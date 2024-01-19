3690e95e1693c617f15fd83d38a7340105cc3394
gauravpandey9129 <154327730+gauravpandey9129@users.noreply.github.com>
2024-01-19 22:05:31 +0530

    Get element by id

---
+++ getElementById.js
@@ -0,0 +1,14 @@
+const header= document.getElementById("header");
+header.style.backgroundColor="green";
+header.style.color="orange";
+header.style.borderBottom= "3px solid orange"
+
+const mainheading=document.getElementById("main-heading");
+mainheading.textContent="Fruit World";
+mainheading.style.color="orange";
+
+const heading=document.getElementById("basket-heading");
+heading.style.color="green"
+
+const thank=document.getElementById("thanks");
+thank.innerHTML="<p>Please visit us again</p>"
